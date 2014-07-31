bd52c95fa3217181e0cab19f371e0a6b7bae5d4f
LifeMushroom <mathmaster01@gmail.com>
2014-07-31 15:41:41 -0400

    add 8jedayoshi's rankset

---
+++ ranksets/yoshi/rankset.php
@@ -0,0 +1,19 @@
+<?php
+
+$ranksetNames["yoshi"] = "Yoshi";
+
+$ranksetData["yoshi"] = array(
+	array("num" => 0, "image" => '', "text" => 'Non-poster Yoshi'),
+	array("num" => 1, "image" => 'yoshiegg.png', "text" => 'Yoshi Egg'),
+	array("num" => 50, "image" => 'yellowyoshiegg.png', "text" => 'Yellow Yoshi Egg'),
+	array("num" => 100, "image" => 'blueyoshiegg.png', "text" => 'Blue Yoshi Egg'),
+	array("num" => 200, "image" => 'redyoshiegg.png', "text" => 'Red Yoshi Egg'),
+	array("num" => 500, "image" => 'kingyoshiegg.png', "text" => 'King Yoshi Egg'),
+	array("num" => 800, "image" => 'yoshi.png', "text" => 'Yoshi'),
+	array("num" => 900, "image" => 'yellowyoshi.png', "text" => 'Yellow Yoshi'),
+	array("num" => 1000, "image" => 'blueyoshi.png', "text" => 'Blue Yoshi'),
+	array("num" => 2000, "image" => 'redyoshi.png', "text" => 'Red Yoshi'),
+	array("num" => 3000, "image" => 'kingyoshi.png', "text" => 'King Yoshi'),
+	array("num" => 5000, "image" => 'rainbowyoshi.png', "text" => 'Rainbow Yoshi'),
+	array("num" => 10000, "image" => 'kingrainbowyoshi.png', "text" => 'King Rainbow Yoshi'),
+);
