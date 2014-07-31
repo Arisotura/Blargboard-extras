12b88ae081554aa8e2485c59c42980b288b196f0
LifeMushroom <mathmaster01@gmail.com>
2014-07-31 15:47:25 -0400

    add hennamann's download page addon

---
+++ pages/downloads.php
@@ -0,0 +1,71 @@
+<!-- Download Page made by Hennamann for Blargboard 1.1 and 1.2
+ Blargboard made by StapleButter. Enjoy! -->
+
+<style>
+  .downloadtable{
+   border: 1px solid #0D462B;
+   border-radius: 20px;
+   }
+
+#downloads {
+    font-family: "Verdana", "Lucida Grande", sans-serif;
+    width: 100%;
+    border-collapse: collapse;
+}
+
+#downloads td, #downloads th {
+    font-size: 1em;
+    border: 1px solid #0D462B;
+    padding: 3px 7px 2px 7px;
+    color: #7BFFAA
+}
+
+#downloads th {
+    font-size: 1.1em;
+    text-align: left;
+    padding-top: 5px;
+    padding-bottom: 4px;
+    background: linear-gradient(#0F5233, #0A3822);
+    color: #FFFFFF;
+}
+
+#downloads tr.alt td {
+    color: #7BFFAA;
+    background-color: #222624;
+}
+</style>
+	<table class="downloadtable" width="1029" border="1" cellspacing="1" cellpadding="0">
+	<table id="downloads">
+		<tr>
+			<th>Download Name</th>
+			<th>Download Link</th>
+			<th>Notes</th>
+		</tr>
+        <tr>
+			<td><b>Add download name here<b></td>
+			<td><b><a href="">Add download link here</a><b></td>
+			<td>Add release notes or something like that here, or if you don't need to post anything here just write n/a</td>
+		</tr>
+			
+		<tr class="alt">
+			<td><b>Add download name here<b></td>
+			<td><b><a href="">Add download link here</a><b></td>
+			<td>Add release notes or something like that here, or if you don't need to post anything here just write n/a</td>
+		</tr>
+		<tr>
+			<td><b>Add download name here<b></td>
+			<td><b><a href="">Add download link here</a><b></td>
+			<td>Add release notes or something like that here, or if you don't need to post anything here just write n/a</td>
+	<tr>	
+		</tr>	
+		<tr class="alt">
+			<td><b>Add download name here<b></td>
+			<td><b><a href="">Add download link here</a><b></td>
+			<td>Add release notes or something like that here, or if you don't need to post anything here just write n/a</td>
+		</tr>
+		<tr>
+			<td><b>Add download name here<b></td>
+			<td><b><a href="">Add download link here</a><b></td>
+			<td>Add release notes or something like that here, or if you don't need to post anything here just write n/a</td>
+		</tr>
+	</table>
