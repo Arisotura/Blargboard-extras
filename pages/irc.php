71e45d0c209b119ea8de2a655952672366bb8878
LifeMushroom <mathmaster01@gmail.com>
2014-09-13 22:06:39 -0400

    Update irc.php

--- pages/irc.php
+++ pages/irc.php
@@ -1,3 +1,5 @@
 <div align="center">
-<iframe width="700" height="500" scrolling="no" src="Which link your irc server gave you to embed a page"></iframe>
+<!--In src, there should be the link your irc server gave you to embed a page. In this example, it is freenode.-->
+<iframe src="http://webchat.freenode.net?uio=d4" width="647" height="400">
+</iframe>
 </div>
