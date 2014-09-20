4aae08a9852ec9a054592a415d37d9eead8662b0
1UpMushroom <angelyoshi34@gmail.com>
2014-09-19 20:23:45 -0700

    Update downloads.php
    <small><small>don't bash me</small></small>

--- pages/downloads.php
+++ pages/downloads.php
@@ -115,13 +115,13 @@
 	<div id="definitions"><div id="titlebar"><center><h3>Icon Definitions:</h3></center></div><i class="fa fa-exclamation-triangle"></i>: Unstable/Dangerous<br>
 	<i class="fa fa-file-code-o"></i>: Script File<br>
 	<i class="fa fa-file-pdf-o"></i>: PDF File<br>
-	<i class="fa fa-file-audio-o"></i>: Audio File(MP3, OGG, etc)<br>
+	<i class="fa fa-file-audio-o"></i>: Audio File(MP3, OGG, MIDI, etc)<br>
 	<i class="fa fa-file-excel-o"></i>: Excel Sheet<br>
 	<i class="fa fa-file-word-o"></i>: Word Document<br>
 	<i class="fa fa-file-powerpoint-o"></i>: Powerpoint Presentation<br>
 	<i class="fa fa-file-image-o"></i>: Image File(JPG, PNG, etc)<br>
 	<i class="fa fa-file-video-o"></i>: Video File(MP4, WMV, etc)<br>
-	<i class="fa fa-file-archive-o"></i>: Archive(Zip, 7Zip, etc)<br>
+	<i class="fa fa-file-archive-o"></i>: Archive(Zip, 7Zip, rar, etc)<br>
 	<!-- Add your own Icon Definitions either using the font awesome icons, or by using your own icons here: VV. Feel free to remove the other ones or replace them!(: -->
 	<br>
 	</div>
@@ -129,5 +129,6 @@
 	<div id="notes"><div id="titlebar"><center><h3>Notes:</h3></center></div>
 	- We are not to be held responsible for any misuse of these downloads, unless otherwise stated.<br>
 	- We are not to be held responsible if these downloads don't work on your system.<br>
-	- We are not to be held responsible if these downloads break your system.
+	- We are not to be held responsible if these downloads break your system.<br>
+	- We are not to be held responsible if these downloads expose a virus to your system.
 	</div>
