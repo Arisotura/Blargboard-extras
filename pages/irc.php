2568edd6e223ce8b5f6abdc697f8840b32e07c89
8JeDaYoshi8 <jedayoshi@gmail.com>
2014-12-05 15:00:04 -0430

    Improving IRC
    Now with KiwiIRC and instructions to connect in IRC clients.

--- pages/irc.php
+++ pages/irc.php
@@ -1,5 +1,9 @@
 <div align="center">
-<!--In src, there should be the link your irc server gave you to embed a page. In this example, it is freenode.-->
-<iframe src="http://webchat.freenode.net?uio=d4" width="647" height="400">
+<!--There's an online IRC client working with KiwiIRC. Edit the stuff for the server and channel, and, if you want, change IRCGuest to something more cool.-->
+<iframe src="https://kiwiirc.com/client/irc.yourserver.com/?nick=IRCGuest?#youchannel" style="border:0; width:100%; height:450px;"></iframe>
 </iframe>
+<br>
+You can join the channel with your IRC Client with this data:<br>
+<tbody><tr class="header1"><th>Channel:</th><th style="width: 20%">Server:</th></tr><tr class="cell1"><td>#yourchannel</td><td><table><tbody><tr>irc.yourserver.com</td></tr></tbody></table></td></tr></tbody></table>
+<br>If you client allows irc:// links, click <a href="#" onclick="irc://irc.yourserver.com/yourchannel">here</a>
 </div>
