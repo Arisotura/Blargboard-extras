085788664cfb154a63b398093e13789bfd001717
8JeDaYoshi8 <jedayoshi@gmail.com>
2014-12-06 17:32:26 -0430

    Another mistake...
    Sorry!

--- pages/irc.php
+++ pages/irc.php
@@ -7,7 +7,8 @@ $title = __("IRC Page");
 <!--IRC page for Blargboard/Blargboard Plus/ABXD by 8JeDaYoshi8.-->
 <iframe src="https://kiwiirc.com/client/irc.yourserver.com/?nick=IRCGuest?#yourchannel" style="border:0; width:100%; height:450px;"></iframe>
 <br>
-If your client allows irc:// links, click <a href="irc://irc.yourserver.com/yourchannel">here</a>
+If you have an IRC Client..:<br>
+If your IRC client allows irc:// links, click <a href="irc://irc.yourserver.com/yourchannel">here</a><br>
 You can join the channel with your IRC Client with this data:<br>
 <tbody><tr class="header1"><th>Channel:</th><th style="width: 20%">Server:</th></tr><tr class="cell1"><td>#yourchannel</td><td><table><tbody><tr>irc.yourserver.com</td></tr></tbody></table></td></tr></tbody></table>
 </div>
