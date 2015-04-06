62639c32470f8b11a98ede72bab900913669e456
Jeff - AlphaTech <alphat3ch@icloud.com>
2015-04-06 12:51:43 -0400

    Update irc.php

--- pages/irc.php
+++ pages/irc.php
@@ -13,6 +13,6 @@ $nameprefix = "IRCGuest";
 <iframe src="https://kiwiirc.com/client/<?php echo $net; ?>/?nick=<?php echo $nameprefix; ?>/?#<?php echo $chan; ?>" style="border:0; width:125%; height:525px;"></iframe>
 <p>If your IRC client allows irc:// links, click <a href="irc://<?php echo $net; ?>/<?php echo $chan; ?>">here</a> to join our channel!</p>
 <p>Channel: #<?php echo $chan; ?></p>
-<p>Server: <?php Echo $net; ?></p>
+<p>Server: <?php echo $net; ?></p>
 </div>
 </html>
