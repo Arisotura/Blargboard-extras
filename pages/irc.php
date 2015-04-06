bfca1246dcc8edf7af77dda6df8f1a5257e5e675
AlphaTech <alphat3ch@icloud.com>
2015-04-06 06:41:41 -0700

    Changes
    Added some variables so pages/irc.php so you only had to put the details
    in there once at the top and added my own themes, black_triforce,
    green_triforce and green_winged_triforce. Enjoy!

--- pages/irc.php
+++ pages/irc.php
@@ -1,15 +1,20 @@
 <?php
 $title = __("IRC Page");
+// The network hostname of your IRC channel
+$net = "irc.yournetwork.net";
+// The actual name of the channel without the beginning #
+$chan = "YourChannel";
+// The name prefix for anyone joining
+$nameprefix = "IRCGuest";
 ?>
 <html>
 <div align="center">
 <center>
-<!--There's an online IRC client working with KiwiIRC. Edit the stuff for the server and channel, and, if you want, change IRCGuest to something more cool.-->
-<iframe src="https://kiwiirc.com/client/irc.yourserver.com/?nick=IRCGuest?#yourchannel" style="border:0; width:125%; height:525px;"></iframe>
+<iframe src="https://kiwiirc.com/client/<?php echo $net; ?>/?nick=<?php echo $nameprefix; ?>/?#<?php echo $chan; ?>" style="border:0; width:125%; height:525px;"></iframe>
 <br>
 If you have an IRC Client..:<br>
-If your IRC client allows irc:// links, click <a href="irc://irc.yourserver.com/yourchannel">here</a><br>
+If your IRC client allows irc:// links, click <a href="irc://<?php echo $net; ?>/<?php echo $chan; ?>">here</a><br>
 You can join the channel with your IRC Client with this data:<br>
-<tbody><tr class="header1"><th>Channel:</th><th style="width: 20%">Server:</th></tr><tr class="cell1"><td><center>#yourchannel</center></td><td><table><tbody><tr><center>irc.yourserver.com</center></td></tr></tbody></table></td></tr></tbody></table>
+<tbody><tr class="header1"><th>Channel:</th><th style="width: 20%">Server:</th></tr><tr class="cell1"><td><center>#<?php echo $chan; ?></center></td><td><table><tbody><tr><center><?php echo $net; ?>0</center></td></tr></tbody></table></td></tr></tbody></table>
 </div>
 </html>
---
+++ themes/black_triforce/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: http://btri.x10.bz");
+exit;
+?>
