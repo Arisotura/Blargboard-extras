b5ad5bc670b6eeb1f8df70a1f72943d61c71850b
Jeff - AlphaTech <alphat3ch@icloud.com>
2015-04-06 12:48:17 -0400

    Update irc.php

--- pages/irc.php
+++ pages/irc.php
@@ -11,10 +11,8 @@ $nameprefix = "IRCGuest";
 <div align="center">
 <center>
 <iframe src="https://kiwiirc.com/client/<?php echo $net; ?>/?nick=<?php echo $nameprefix; ?>/?#<?php echo $chan; ?>" style="border:0; width:125%; height:525px;"></iframe>
-<br>
-If you have an IRC Client..:<br>
-If your IRC client allows irc:// links, click <a href="irc://<?php echo $net; ?>/<?php echo $chan; ?>">here</a><br>
-You can join the channel with your IRC Client with this data:<br>
-<tbody><tr class="header1"><th>Channel:</th><th style="width: 20%">Server:</th></tr><tr class="cell1"><td><center>#<?php echo $chan; ?></center></td><td><table><tbody><tr><center><?php echo $net; ?>0</center></td></tr></tbody></table></td></tr></tbody></table>
+<br>If your IRC client allows irc:// links, click <a href="irc://<?php echo $net; ?>/<?php echo $chan; ?>">here</a> to join our channel!<br>
+Channel: #<?php echo $chan; ?>
+Server: <?php Echo $net; ?>
 </div>
 </html>
